4457d7432cf9cf838708c5e1a2768d0cd80f2d59
pamun1 <108357749+pamun1@users.noreply.github.com>
2022-09-29 11:17:35 -0400

    added log level setting to examples

--- python/examples/shuttle.py
+++ python/examples/shuttle.py
@@ -1,12 +1,16 @@
+import logging # Required to modify the log level
+
 from multimodalsim.optimization.dispatcher import ShuttleGreedyDispatcher
 from multimodalsim.optimization.optimization import Optimization
-from multimodalsim.optimization.splitter import OneLegSplitter
 from multimodalsim.reader.data_reader import ShuttleDataReader
 from multimodalsim.simulator.network import create_graph
 from multimodalsim.simulator.simulation import Simulation
 from multimodalsim.visualizer.visualizer import ConsoleVisualizer
 
 if __name__ == '__main__':
+
+    # To modify the log level (at INFO, by default)
+    logging.getLogger().setLevel(logging.DEBUG)
 
     # Read input data from files
     requests_file_path = "../../data/shuttle/test0_shuttle/requests.csv"
@@ -22,9 +26,10 @@ if __name__ == '__main__':
     g = create_graph(nodes)
 
     # Initialize the optimizer.
-    splitter = OneLegSplitter()
     dispatcher = ShuttleGreedyDispatcher(g)
-    opt = Optimization(dispatcher, splitter)
+
+    # OneLegSplitter is used by default
+    opt = Optimization(dispatcher)
 
     # Initialize the visualizer.
     visualizer = ConsoleVisualizer()
