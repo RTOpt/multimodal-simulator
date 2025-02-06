a9e27053d5af715866ae421ea94646b0aa87cbd7
LauraKolcheva <laura.kolcheva@polymtl.ca>
2025-02-06 16:11:17 -0500

    test toutes lignes/dir + suppression lignes problematiques

--- python/examples/fixed_line/fixed_line_transfer_synchro_testfile.py
+++ python/examples/fixed_line/fixed_line_transfer_synchro_testfile.py
@@ -1,11 +1,11 @@
 ### DO NOT CHANGE THESE LINES: Parameters are auto-filled in stl_gtfs_parameter_parser_and_test_file_generator.py
 ### BEGINNING OF PARAMETERS ###
 import os
-gtfs_folder_path = os.path.join("data","fixed_line","gtfs","gtfs2019-11-25-TestInstanceDurationCASPT_NEW")
+gtfs_folder_path = os.path.join("data","fixed_line","gtfs","gtfs2019-11-25-LargeInstanceAll")
 requests_file_path = os.path.join(gtfs_folder_path,"requests.csv")
-output_folder_path = os.path.join("output","fixed_line","gtfs","gtfs2019-11-25_TestInstanceDurationCASPT_NEW")
-output_folder_name = "gtfs2019-11-25_TestInstanceDurationCASPT_NEW"
-routes_to_optimize_names = ['17N', '151S', '26E', '42E','56E']
+output_folder_path = os.path.join("output","fixed_line","gtfs","gtfs2019-11-25_LargeInstanceAll")
+output_folder_name = "gtfs2019-11-25_LargeInstanceAll"
+routes_to_optimize_names =  ['144E', '144O', '20E', '20O', '222E', '222O', '22E', '22O', '24E', '24O', '252E', '252O', '26E', '26O', '42E', '42O', '52E', '52O', '56E', '56O', '60E', '60O', '66E', '66O', '70E', '70O', '74E', '74O', '76E', '76O', '942E', '942O', '151S', '151N', '17S', '17N', '27S', '27N', '33S', '33N', '37S', '37N', '41S', '41N', '43S', '43N', '45S', '45N', '46S', '46N', '55S', '55N', '61S', '61N', '63S', '63N', '65S', '65N', '901S', '901N', '902S', '902N', '903S', '903N', '925S', '925N']
 algo = 0
 sp = False
 ss = False
