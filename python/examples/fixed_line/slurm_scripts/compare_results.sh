b6f42415a78c6438925c7180cc53f11cff77bb3c
LauraKolcheva <laura.kolcheva@polymtl.ca>
2025-03-05 12:59:09 -0500

    fix sh script

--- python/examples/fixed_line/slurm_scripts/compare_results.sh
+++ python/examples/fixed_line/slurm_scripts/compare_results.sh
@@ -2,8 +2,8 @@
 #SBATCH --mem=10G
 #SBATCH --time=10:00
 #SBATCH --partition=optimum
-#SBATCH --output=python/examples/python/examples/fixed_line/solution_comparison_slurm_output_%A_%a.out
-#SBATCH --error=python/examples/python/examples/fixed_line/solution_comparison_slurm_error_%A_%a.err
+#SBATCH --output=python/examples/fixed_line/solution_comparison_slurm_output_%A_%a.out
+#SBATCH --error=python/examples/fixed_line/solution_comparison_slurm_error_%A_%a.err
 
 # Change to the correct working directory
 cd /home/kollau/Recherche_Kolcheva/Simulator
